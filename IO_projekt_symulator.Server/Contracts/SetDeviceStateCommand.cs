eeeaa9862cabb1534f6c5c7ec20d90fc1c1718a4
Dominika Maćkowiak <dm311397@student.polsl.pl>
2025-12-05 13:25:40 +0100

    dodalam MassTransit z MQrabbit -> wyamagan implemnetacja tego systemu kolejkowania po stronie panlu sterowania

---
+++ IO_projekt_symulator.Server/Contracts/SetDeviceStateCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace IO_projekt_symulator.Server.Contracts
+{
+    // Ta klasa definiuje, co Panel może do Ciebie wysłać
+    public class SetDeviceStateCommand
+    {
+        public Guid DeviceId { get; set; }
+        public double Value { get; set; }
+        // Opcjonalnie możesz dodać Unit, jeśli ustalicie, że Panel może zmieniać jednostki
+    }
+}
--- IO_projekt_symulator.Server/Program.cs
+++ IO_projekt_symulator.Server/Program.cs
@@ -1,10 +1,38 @@
 using IO_projekt_symulator.Server.Hubs;
+using MassTransit;
+using IO_projekt_symulator.Server.Consumers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // 1. DODAJ SIGNALR DO KONTENERA
 builder.Services.AddSignalR();
+
+builder.Services.AddSignalR();
+
+// --- KONFIGURACJA MASSTRANSIT (RABBITMQ) ---
+builder.Services.AddMassTransit(x =>
+{
+    // 1. Mówimy, ¿e mamy takiego konsumenta
+    x.AddConsumer<DeviceCommandConsumer>();
+
+    // 2. Konfigurujemy po³¹czenie z RabbitMQ
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        // Tutaj podajemy namiary na serwer RabbitMQ.
+        // Jeœli uruchomisz go lokalnie na Dockerze, to s¹ domyœlne ustawienia:
+        cfg.Host("localhost", "/", h => {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        // To automatycznie tworzy kolejki i podpina konsumenta
+        cfg.ConfigureEndpoints(context);
+    });
+});
+// ---------------------------------------------
+
+
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IO_projekt_symulator.Server.Services.IVirtualDeviceService, IO_projekt_symulator.Server.Services.VirtualDeviceService>();
 // Rejestrujemy nasza usluge dzialajaca w tle
